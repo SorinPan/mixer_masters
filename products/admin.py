41a7f2ffd97b32bae5a407f7370eb38231245fff
Sorin Panait <sorin.panait0@gmail.com>
2024-01-09 03:54:17 +0000

    Customise the admin panel)

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('friendly_name', 'name')
+    list_filter = ['name',]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku', 'name', 'stock', 'price', 'category',
+                    'rating', 'image')
+    ordering = ('sku',)
+    search_fields = ('name',)
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/test_models.py
+++ products/test_models.py
@@ -1,4 +1,45 @@
 from django.test import TestCase
 from .models import Category, Product
+from decimal import Decimal
 
+class TestCategoryModel(TestCase):
+
+    def setUp(self):
+        Category.objects.create(name='test_category', friendly_name='Test Category')
+
+    def test_category_string(self):
+        category = Category.objects.get(name='test_category')
+        self.assertEqual(str(category), 'test_category')
+
+    def test_get_friendly_name(self):
+        category = Category.objects.get(name='test_category')
+        self.assertEqual(category.get_friendly_name(), 'Test Category')
+
+
+class TestProductModel(TestCase):
+
+    def setUp(self):
+        Category.objects.create(name='Test Category', friendly_name='Test Friendly Name')
+        Product.objects.create(
+            category=Category.objects.get(name='Test Category'),
+            slug='test-product',
+            sku='TEST123',
+            name='Test Product',
+            price=9.99,
+            rating=5,
+            description='Test Description',
+            stock=10,
+            features='Test Features',
+            image_url='http://example.com/image.jpg',
+            image='test_image.webp'
+        )
+
+    def test_product_creation(self):
+        product = Product.objects.get(slug='test-product')
+        self.assertEqual(product.name, 'Test Product')
+        self.assertEqual(product.price, Decimal('9.99'))
+
+    def test_product_str(self):
+        product = Product.objects.get(slug='test-product')
+        self.assertEqual(str(product), 'Test Product')
 
