1791faf830b27f659a75e20bb04e801d4495007f
Sorin Panait <sorin.panait0@gmail.com>
2024-01-10 21:07:33 +0000

    Add product detail view and template, related to #3

--- products/test_views.py
+++ products/test_views.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import Category, Product
+from django.urls import reverse
 
-# Create your tests here.
+
+class ProductViewsTest(TestCase):
+
+    def setUp(self):
+        category = Category.objects.create(name='test_category', friendly_name='Test Category')
+        Product.objects.create(
+            category=category,
+            slug='test-product',
+            name='Test Product',
+            price=9.99,
+            stock=10,
+            description='Test Description',
+            features='Test Features',
+            image_url='http://example.com/image.jpg',
+            image='test_image.jpg'
+        )
+
+    def test_products_view(self):
+        response = self.client.get(reverse('products'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/products.html')
+        self.assertIn('products', response.context)
+
+    def test_product_detail_view(self):
+        product = Product.objects.get(slug='test-product')
+        response = self.client.get(reverse('product_detail', args=[product.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/product_detail.html')
+        self.assertEqual(response.context['product'], product)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
+
 def all_products(request):
-    """ A view to return all products on the site """
+    """
+    A view to return all products on the site
+    """
 
     products = Product.objects.all()
 
@@ -11,3 +14,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to show a selected product details
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
