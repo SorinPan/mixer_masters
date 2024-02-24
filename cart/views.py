910e4f77597fe185ab246abca0992f67eb459687
Sorin Panait <sorin.panait0@gmail.com>
2024-02-24 14:46:56 +0000

    Create bootstrap toasts

--- cart/views.py
+++ cart/views.py
@@ -15,14 +15,18 @@ def add_to_cart(request, item_id):
     Adds the quantity of an item to the shoping cart
     """
     
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
